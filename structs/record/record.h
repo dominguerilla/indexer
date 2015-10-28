7ba112803fe65025a8b38f56707e6ffdabc8cbea
dominguerilla <dominguerilla@gmail.com>
2015-10-28 19:34:36 -0400

    Updated notes.pdf

---
+++ structs/record/record.h
@@ -0,0 +1,21 @@
+#IFNDEF RECORD_H
+#DEFINE RECORD_H
+
+/*
+*
+* The Record struct represents a single file that some token appears in. 
+* It holds the name of the file and the number of times it appears in it.
+*
+*/
+typedef struct Record_{
+	char* filename;
+	int instances;
+} Record;
+
+
+/*
+*
+* Utility functions if necessary
+*
+*/
+#ENDIF
---
+++ structs/table/table.h
@@ -0,0 +1,10 @@
+#IFNDEF TABLE_H
+#DEFINE TABLE_H
+
+typedef struct table_{
+
+	
+
+} Table;
+
+#ENDIF
